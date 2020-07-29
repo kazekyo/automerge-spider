47080558b8231fc1e3dca1bb161d041bed77d8f7
kazekyo <k.hukusui@gmail.com>
2020-07-29 17:29:11 +0900

    update project

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  verbose: true,
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
